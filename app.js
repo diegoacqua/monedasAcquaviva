e045db690ad094bd95a2a120bd3151f73f4a8a94
Diego Acquaviva <diegojacqua@gmail.com>
2022-08-31 18:57:02 -0400

    le agregue localStorage y JSON al codigo, falta solucionar un problema

--- app.js
+++ app.js
@@ -1,44 +1,70 @@
-let form = document.getElementById("form");
-form.addEventListener("submit", prueba);
+class Conversion {
+  constructor(cantidad, moneda) {
+    this.cantidad = cantidad;
+    this.moneda = moneda;
+  }
+}
 
+let calcPrevios = [];
+
+let form = document.getElementById("form");
 let div = document.getElementById("container");
+let divPrevios = document.getElementById("previos");
+let btn = document.getElementById("btn");
+
+form.addEventListener("submit", prueba);
+btn.addEventListener("click", xd);
 
 function prueba(e) {
   e.preventDefault();
   let datos = e.target;
-  let pesos = datos.children[0].value;
-  let moneda = datos.children[1].value;
+  let pesos = datos.children[1].value;
+  let moneda = datos.children[3].value;
   conversor(pesos, moneda);
 }
 
 function conversor(cantidad, moneda) {
   if (!isNaN(cantidad)) {
     switch (moneda) {
-      case "dolar":
-        let dolar = cantidad * 0.0076 + " dolares!";
-        return imprimir(dolar);
-      case "yen":
-        let yen = cantidad * 1.01 + " yenes!";
-        return imprimir(yen);
-      case "euro":
-        let euro = cantidad * 0.0074 + " euros!";
-        return imprimir(euro);
-      case "libra esterlina":
-        let libra = cantidad * 0.006 + " libras esterlinas!";
-        return imprimir(libra);
-      case "sol":
-        let sol = cantidad * 0.03 + " soles!";
-        return imprimir(sol);
+      case "dolares":
+        let dolar = cantidad * 0.0076;
+        return imprimir(dolar, moneda);
+      case "yenes":
+        let yen = cantidad * 1.01;
+        return imprimir(yen, moneda);
+      case "euros":
+        let euro = cantidad * 0.0074;
+        return imprimir(euro, moneda);
+      case "libras esterlinas":
+        let libra = cantidad * 0.006;
+        return imprimir(libra, moneda);
+      case "soles":
+        let sol = cantidad * 0.03;
+        return imprimir(sol, moneda);
       default:
         return alert("ingrese una moneda valida");
     }
   }
 }
 
-function imprimir(resultado) {
+function imprimir(resultado, moneda) {
   let p = document.createElement("p");
-  p.innerHTML = `Tendrias ${resultado}`;
+  p.innerHTML = `Tendrias ${resultado} ${moneda}`;
   div.appendChild(p);
+  calcPrevios.push(new Conversion(resultado, moneda));
+  const convertirEnString = JSON.stringify(calcPrevios);
+  localStorage.setItem("calcPrevio", convertirEnString);
+  console.log(calcPrevios);
+}
+
+function xd() {
+  let calcPrevio = JSON.parse(localStorage.getItem("calcPrevio"));
+  console.log(calcPrevio);
+  let p = document.createElement("p");
+  for (const calculo of calcPrevio) {
+    p.innerHTML = `${calculo.cantidad} ${calculo.moneda}`;
+    divPrevios.appendChild(p);
+  }
 }
 
 // let calcPrevios = [];
