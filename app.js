de0e2830acc72c21381f08fdb6d54d92855df042
Diego Acquaviva <diegojacqua@gmail.com>
2022-08-31 19:38:56 -0400

    se le soluciono el problema a imprimirPrev

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let divPrevios = document.getElementById("previos");
 let btn = document.getElementById("btn");
 
 form.addEventListener("submit", prueba);
-btn.addEventListener("click", xd);
+btn.addEventListener("click", imprimirPrev);
 
 function prueba(e) {
   e.preventDefault();
@@ -57,78 +57,11 @@ function imprimir(resultado, moneda) {
   console.log(calcPrevios);
 }
 
-function xd() {
+function imprimirPrev() {
   let calcPrevio = JSON.parse(localStorage.getItem("calcPrevio"));
   console.log(calcPrevio);
-  let p = document.createElement("p");
   for (const calculo of calcPrevio) {
-    p.innerHTML = `${calculo.cantidad} ${calculo.moneda}`;
-    divPrevios.appendChild(p);
+    divPrevios.innerHTML += `<p>${calculo.cantidad} ${calculo.moneda}</p>`;
+    divPrevios.append();
   }
 }
-
-// let calcPrevios = [];
-
-// function conversor(cantidad) {
-//   if (!isNaN(cantidad)) {
-//     let moneda = prompt(
-//       "Ingrese la moneda a la que desea convertir:\nDolar\nYen\nEuro\nLibra Esterlina\nSol"
-//     ).toLowerCase();
-//     switch (moneda) {
-//       case "dolar":
-//         let dolar = cantidad * 0.0076 + " dolares!";
-//         calcPrevios.push(dolar);
-//         return "Tendrias " + dolar;
-//       case "yen":
-//         let yen = cantidad * 1.01 + " yenes!";
-//         calcPrevios.push(yen);
-//         return "Tendrias " + yen;
-//       case "euro":
-//         let euro = cantidad * 0.0074 + " euros!";
-//         calcPrevios.push(euro);
-//         return "Tendrias " + euro;
-//       case "libra esterlina":
-//         let libra = cantidad * 0.006 + " libras esterlinas!";
-//         calcPrevios.push(libra);
-//         return "Tendrias " + libra;
-//       case "sol":
-//         let sol = cantidad * 0.03 + " soles!";
-//         calcPrevios.push(sol);
-//         return "Tendrias " + sol;
-//       default:
-//         return "ingrese una moneda valida";
-//     }
-//   } else {
-//     return "Ingrese un numero";
-//   }
-// }
-
-// let pesos = Number(prompt("Ingrese la cantidad de pesos que quiere convertir"));
-// alert(conversor(pesos));
-
-// let nuevo = prompt(
-//   "Desea hacer otro calculo? \n Y = si\n N = no"
-// ).toLowerCase();
-
-// do {
-//   if (nuevo == "y") {
-//     pesos = Number(prompt("Ingrese la cantidad de pesos que quiere convertir"));
-//     alert(conversor(pesos));
-//     nuevo = prompt(
-//       "Desea hacer otro calculo? \n Y = si\n N = no"
-//     ).toLowerCase();
-//   }
-// } while (nuevo == "y");
-
-// if (nuevo != "y") {
-//   let previo = prompt(
-//     "Desea ver sus calculos previos? \n Y = si\n N = no"
-//   ).toLowerCase();
-//   if (previo == "y") {
-//     for (const cuenta of calcPrevios) {
-//       alert("Tu calculo anterior fue " + cuenta);
-//     }
-//   } else {
-//     alert("Que tenga un buen dia!");
-//   }
-// }
